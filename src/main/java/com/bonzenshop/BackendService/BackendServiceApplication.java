0cce23b3cf27d5e938d8a4ec88087c8668b0a2cc
DanielWunder <41953388+DanielWunder@users.noreply.github.com>
2019-10-16 17:01:38 +0200

    prepared backend for product list in frontend

--- src/main/java/com/bonzenshop/BackendService/BackendServiceApplication.java
+++ src/main/java/com/bonzenshop/BackendService/BackendServiceApplication.java
@@ -3,8 +3,24 @@ package com.bonzenshop.BackendService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class BackendServiceApplication {
+
+	private static Connection con;
+
+	private void getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("org.sqlite.JDBC");
+		con = DriverManager.getConnection("jdbc:sqlite:bonzenshopdb.sqlite");
+		//initialise();
+	}
+
+	private void intialise(){
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendServiceApplication.class, args);
--- src/main/java/com/bonzenshop/BackendService/controller/ShopController.java
+++ src/main/java/com/bonzenshop/BackendService/controller/ShopController.java
@@ -2,12 +2,14 @@ package com.bonzenshop.BackendService.controller;
 
 import com.bonzenshop.BackendService.model.Product;
 import com.bonzenshop.BackendService.service.ProductService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ShopController {
 
