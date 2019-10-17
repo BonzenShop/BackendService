0a15e465cd9bd85e7e696dfdcdc70fd589c5e9c4
DanielWunder <41953388+DanielWunder@users.noreply.github.com>
2019-10-17 14:59:44 +0200

    added sqlite driver and initial setup of the embedded sqlite database

--- src/main/java/com/bonzenshop/BackendService/BackendServiceApplication.java
+++ src/main/java/com/bonzenshop/BackendService/BackendServiceApplication.java
@@ -3,6 +3,7 @@ package com.bonzenshop.BackendService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,20 +11,28 @@ import java.sql.SQLException;
 @SpringBootApplication
 public class BackendServiceApplication {
 
+    public static final String DRIVER = "org.sqlite.JDBC";
+    public static final String JDBC_URL = "jdbc:sqlite:bonzenshopdb.sqlite";
 	private static Connection con;
 
-	private void getConnection() throws ClassNotFoundException, SQLException {
-		Class.forName("org.sqlite.JDBC");
-		con = DriverManager.getConnection("jdbc:sqlite:bonzenshopdb.sqlite");
-		//initialise();
+	private static void getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName(DRIVER);
+
+		//checking if database already exists
+		File db = new File("bonzenshopdb.sqlite");
+		if(!db.exists()){
+			con = DriverManager.getConnection(JDBC_URL);
+			//TODO: create tables and fill with data
+			con.createStatement().execute("CREATE TABLE test(id varchar(20), name varchar(20))");
+			con.createStatement().execute("insert into test values ('id', 'test'),('id2', 'test2')");
+		}else{
+			con = DriverManager.getConnection(JDBC_URL);
+		}
 	}
 
-	private void intialise(){
-
-	}
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		SpringApplication.run(BackendServiceApplication.class, args);
+		getConnection();
 	}
 
 }
